55a969011465e693c623f6673b180f19df3c0e97
Nicolas <bourasseaun@gmail.com>
2021-03-24 10:38:51 +0100

    BUG2_FixedMethodAdd
    Correction de la method Add (-1 sur les HowManyPlayer)

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 
 public class Game {
     ArrayList players = new ArrayList();
-    int[] places = new int[7];
-    int[] purses  = new int[7];
-    boolean[] inPenaltyBox  = new boolean[7];
+    int[] places = new int[6];
+    int[] purses  = new int[6];
+    boolean[] inPenaltyBox  = new boolean[6];
     
     LinkedList popQuestions = new LinkedList();
     LinkedList scienceQuestions = new LinkedList();
@@ -38,9 +38,9 @@ public class Game {
 		
 		if(howManyPlayers() <= 5 ) {
 			players.add(playerName);
-		    places[howManyPlayers()] = 0;
-		    purses[howManyPlayers()] = 0;
-		    inPenaltyBox[howManyPlayers()] = false;
+		    places[howManyPlayers() -1] = 0;
+		    purses[howManyPlayers() -1] = 0;
+		    inPenaltyBox[howManyPlayers() -1] = false;
 		    
 		    System.out.println(playerName + " was added");
 		    System.out.println("They are player number " + players.size());
